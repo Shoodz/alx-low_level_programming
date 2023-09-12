67c087e92f9a11795ad1b129b4dbcb07db8bd3ec
shahd <shoodyomer22214@gmail.com>
2023-09-12 15:29:03 -0700

    d

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+  * main - the main printing funtion
+  * less than 4000000.
+  * Return: 0.
+  */
+
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
