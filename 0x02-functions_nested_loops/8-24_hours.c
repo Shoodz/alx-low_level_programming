d1861eeeac1250da2a2f5f39104bedd1a4f2e935
shahd <shoodyomer22214@gmail.com>
2023-09-12 13:08:34 -0700

    j

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - print last digit of a number.
- *
- * @n: takes number input
- *
- * Return: lastDidgit
-*/
-
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
 int print_last_digit(int n)
 {
-	int lastDigit;
+	int a;
 
 	if (n < 0)
-		lastDigit = -1 * (n % 10);
-	else
-		lastDigit = n % 10;
+	n = -n;
 
-	_putchar(lastDigit + 48);
-	return (lastDigit);
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
