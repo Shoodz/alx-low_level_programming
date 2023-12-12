47159075bb00b7d0fae75dba3ed7e91280cd6454
shahd <shoodyomer22214@gmail.com>
2023-12-11 16:38:10 -0800

    L

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/shoodz/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/libgiga.so
+export LD_PRELOAD=/tmp/iwin.so
